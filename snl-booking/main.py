5df0f7f0185a178660bf5a5f121b53d4ee958b4d
jsyang <jsyang.ca@gmail.com>
2012-02-24 13:42:11 -0500

    building models out

--- snl-booking/main.py
+++ snl-booking/main.py
@@ -1,30 +1,89 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-from google.appengine.ext import webapp2
-from google.appengine.ext.webapp import util
 
+import datetime
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+
+################################################################################
+# Models.
+
+class Appointment(db.Model):
+    date=db.DateTimeProperty()
+    isWalkIn=db.BooleanProperty(required=True)
+    size=db.IntegerProperty(required=True)
+    name=db.StringProperty(required=True)
+    phone=db.PhoneNumberProperty(required=True)
+    email=db.EmailProperty(required=True)
+    remindSameDay=db.BooleanProperty()
+    remindSameWeek=db.BooleanProperty()
+    notes=db.StringProperty()
+
+    def __str__(self):
+        return  "Appointment for "+self.name+". A "+\
+                ("walk-in" if self.isWalkIn==True else "reserved")+\
+                " party, size of "+str(self.size)+". "+\
+                ("They want a same-day reminder. " if self.remindSameDay==True else "")+\
+                ("They want a same-week reminder. " if self.remindSameWeek==True else "")+\
+                "Let them know at "+self.email+" or "+self.phone+". "+\
+                (("Additionally, they said: "+self.notes) if len(self.notes) else "")
+
+################################################################################
+# Controllers.
+
+class ErrorCodes:
+    class json:
+        pass
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        GET=self.request.get
+        secretkey=GET('key')
+        #self.response.out.write('the secret key is: '+secretkey)
+        self.response.out.write('GET stuff: '+secretkey)
 
+class ShowBooking(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hello wofsdfsdfsdrld!')
+
+'''
+http://localhost:8080/TryBooking?API_KEY=abc&size=12&iswalkin=true&name=Joe Smith&phone=(141)123-4189&email=joe@home.com
+http://localhost:8080/TryBooking?API_KEY=abc&size=12&iswalkin=true&name=Joe Smith&phone=(14fdfd123-4189&email=joe@home.com
+http://localhost:8080/TryBooking?API_KEY=abc&size=12&iswalkin=true&name=Joe Smith&phone=(141)123-4189&email=joe@home.com&date=Fri Feb 24 2012 13:40:02 GMT-0500 (EST)
+'''
+
+class TryBooking(webapp.RequestHandler):
+    def get(self):
+        GET=self.request.get
+        if GET('API_KEY')!='abc': raise Exception("Invalid API key!")
+        if  len(GET('iswalkin')) * \
+            len(GET('size')) * \
+            len(GET('name')) * \
+            len(GET('phone')) * \
+            len(GET('email')) == 0 :
+            raise Exception("Required parameters missing!")
+            
+        a=Appointment(date=             GET('date'),
+                      isWalkIn=         bool(GET('iswalkin')),
+                      size=             int(GET('size')),
+                      name=             GET('name'),
+                      phone=            GET('phone'),
+                      email=            GET('email'),
+                      remindSameDay=    bool(GET('sameday')),
+                      remindSameWeek=   bool(GET('sameweek')),
+                      notes=            GET('notes'))
+        self.response.out.write(a)
+
+################################################################################
+
+application = webapp.WSGIApplication([
+    ('/', MainPage),
+    ('/ShowBooking', ShowBooking),
+    ('/TryBooking', TryBooking),
+], debug=True)
 
 def main():
-    application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+    
     util.run_wsgi_app(application)
 
 
